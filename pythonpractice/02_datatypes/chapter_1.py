32c8f4542ce1119088122334e91eac49839275b9
rishijha390 <rishijha390@gmail.com>
2025-09-13 23:09:30 +0530

    added folder datatypes to the python practice folder

---
+++ pythonpractice/02_datatypes/chapter_1.py
@@ -0,0 +1,8 @@
+sugar_amount = 2
+print(f"Initial sugar: {sugar_amount}")
+
+sugar_amount = 12
+print(f"Initial sugar: {sugar_amount}")
+
+print(f"Identity of 2 : {id(2)}")
+print(f"Identity of 12 : {id(12)}")
