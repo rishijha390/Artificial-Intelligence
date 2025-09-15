a0965b7de8c48ed4982efa6d94f28a1412c1c6d4
rishijha390 <rishijha390@gmail.com>
2025-09-15 21:01:36 +0530

    added files to the datatype folder

---
+++ pythonpractice/02_datatypes/chapter_8.py
@@ -0,0 +1,35 @@
+ingredients = ["Water","milk","tea leaves"]
+ingredients.append("sugar")
+print(f"Ingredients in the tea are - {ingredients}")
+
+ingredients.remove("milk")
+print(f"New ingridients for making tea are - {ingredients}")
+
+
+spice_ingredients = ["cardamon","cinnamon"]
+
+tea_ingredients = ["tea leaves" , " sugar"]
+
+tea_ingredients.extend(spice_ingredients)
+print(f"chai : {tea_ingredients}")
+
+tea_ingredients.insert(2,"black tea")
+print(f"new ingridients are: {tea_ingredients}")
+
+
+last_added = tea_ingredients.pop()
+print(f"item removed is: {last_added}")
+print(f"new ingridients are: {tea_ingredients}")
+
+tea_ingredients.reverse()
+print(f"ingridents in reverse order: {tea_ingredients}")
+
+tea_ingredients.sort()
+print(f"sorted ingridients are: {tea_ingredients}")
+
+sugar_levels = [1,2,3,4,5]
+print(f"maximum sugar level is : {max(sugar_levels)}")
+print(f"maximum sugar level is : {min(sugar_levels)}")
+
+
+
