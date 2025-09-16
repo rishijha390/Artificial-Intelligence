60f63067e20a4ee1b7d752ac3bcdbc8225bd1824
rishijha390 <rishijha390@gmail.com>
2025-09-16 22:54:13 +0530

    adding files  to the conditionals folder

---
+++ pythonpractice/03_conditionals/snac_suggestion.py
@@ -0,0 +1,6 @@
+snacks = input("Enter you preferrred snacks: ").lower()
+
+if snacks == "cookies" or snacks == "samosa":
+    print(f"Great choice! We'll serve you {snacks}")
+else:
+    print(f"Sorry,we only serve cookies or samosa with tea")
