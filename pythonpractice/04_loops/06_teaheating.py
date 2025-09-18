ec0a21aedffcc9b7ac0e4a4eb205f40443184f3a
rishijha390 <rishijha390@gmail.com>
2025-09-18 20:48:28 +0530

    add files to the loops folder

---
+++ pythonpractice/04_loops/06_teaheating.py
@@ -0,0 +1,8 @@
+tea_heating = 40
+while tea_heating < 100:
+    tea_heating += 15
+    print(f"Current temperture is {tea_heating}")
+
+print("Tea is ready to boil")
+
+    
---
+++ pythonpractice/04_loops/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+tea_heating = 40
+while(tea_heating <= 100):
+    print(f"Current temperture is {tea_heating}")
+    tea_heating += 15
+
+    
