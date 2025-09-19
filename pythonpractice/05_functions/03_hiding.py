e95a12df19a72b2352d0e36183efd3622d2b0f96
rishijha390 <rishijha390@gmail.com>
2025-09-19 23:19:17 +0530

    added files to the functions folder

---
+++ pythonpractice/05_functions/03_hiding.py
@@ -0,0 +1,16 @@
+def get_input():
+    print(f"Getting user input")
+
+def validate_input():
+    print(f"Validating user input")
+
+def save_to_db():
+    print(f"Saving to the database")
+
+def register_user():
+    get_input()
+    validate_input()
+    save_to_db()
+    print("User registration successfull")
+
+register_user()
