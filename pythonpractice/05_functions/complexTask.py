62b3cb00dbf05d3ea83ea8e0a2d88c7dc0aeb3e3
rishijha390 <rishijha390@gmail.com>
2025-09-19 20:40:10 +0530

    added files to the functions folder

---
+++ pythonpractice/05_functions/complexTask.py
@@ -0,0 +1,8 @@
+def fetch_sales(order_received , order_delivered):
+    print(f"Total number of ordered received are {order_received} and delivered are {order_delivered}")
+
+def filter_valid_orders(valid_orders , fake_orders):
+    print(f"Total valid orders were { valid_orders} and rejected orders were {fake_orders}")
+
+def summarize_data(original_order , order_delivered):
+    print(f"Order received {original_order} and delivers were {order_delivered}")
---
+++ pythonpractice/05_functions/duplicate.py
@@ -0,0 +1,6 @@
+def print_order(name , chai_type):
+    print(f"{name} ordered {chai_type} chai")
+
+print_order("Rishi", "masala")
+print_order("Manuj" , "Tulsi")
+print_order("Divyanshu" , "Ginger")
