fce35202fe4c91ea30b48d23dec169306f182cf9
rishijha390 <rishijha390@gmail.com>
2025-09-19 22:56:09 +0530

    added files to the functions folder

--- pythonpractice/05_functions/complexTask.py
+++ pythonpractice/05_functions/complexTask.py
@@ -1,8 +1,23 @@
-def fetch_sales(order_received , order_delivered):
-    print(f"Total number of ordered received are {order_received} and delivered are {order_delivered}")
+def fetch_sales():
+    return [
+        {"id":1 , "status":"delivered"},
+        {"id":2 , "status":"fake"},
+        {"id":3 , "status":"delivered"}
+    ]
 
-def filter_valid_orders(valid_orders , fake_orders):
-    print(f"Total valid orders were { valid_orders} and rejected orders were {fake_orders}")
+def filter_valid_orders(sales):
+    return [order for order in sales if order["status"] == "delivered"]
 
-def summarize_data(original_order , order_delivered):
-    print(f"Order received {original_order} and delivers were {order_delivered}")
+def summarize_data(valid_orders):
+    return{
+        "total orders": len(valid_orders),
+        "delivered": len(valid_orders)
+    }
+
+def generate_report():
+    sales = fetch_sales()
+    valid_orders = filter_valid_orders(sales)
+    summary = summarize_data(valid_orders)
+    print("Monthly Report:",summary)
+
+generate_report()
