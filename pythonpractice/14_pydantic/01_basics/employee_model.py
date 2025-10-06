85a5f6ce26b807583b794eab23e2e5b3bb7e4fbf
rishijha390 <rishijha390@gmail.com>
2025-10-07 00:14:05 +0530

    updated the employee_model file

--- pythonpractice/14_pydantic/01_basics/employee_model.py
+++ pythonpractice/14_pydantic/01_basics/employee_model.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
+import re
 
 class Employee(BaseModel):
     id: int
@@ -15,5 +16,26 @@ class Employee(BaseModel):
     salary: float = Field(
         ...,
         ge=10000
-        
+
     )
+
+class User(BaseModel):
+    email : str = Field( ...,pattern=r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$' )
+    phone: str = Field(...,pattern=r'^\+?[1-9]\d{1,14}$')
+    age: int = Field(
+        ...,
+        ge=0,
+        le=150,
+        description="Age in years",
+    )
+    discount: float = Field(
+        ...,
+        ge=0,
+        le=100,
+        description="Discount percentage",
+    )
+
+
+u = User(email="rishi.jha@gmail.com", phone="+919876543210", age=28, discount=10)
+print(u)
+
