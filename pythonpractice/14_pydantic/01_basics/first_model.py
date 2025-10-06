e1c8ad9c0ffd61e26efcb153241847a55d976646
rishijha390 <rishijha390@gmail.com>
2025-10-06 22:57:21 +0530

    added files to the pydantic folder

--- pythonpractice/14_pydantic/01_basics/first_model.py
+++ pythonpractice/14_pydantic/01_basics/first_model.py
@@ -1,1 +1,12 @@
-print("start of pydantic journey")
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+
+input_data = {'id':1,'name':'Rishi Jha','is_active':True}
+
+user = User(**input_data)
+print(user)
+
