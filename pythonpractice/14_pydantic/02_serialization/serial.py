b00dacc727c4ea003566f97be251a00e839a1804
rishijha390 <rishijha390@gmail.com>
2025-10-08 23:57:44 +0530

    added files to the pydantic folder

---
+++ pythonpractice/14_pydantic/02_serialization/serial.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, ConfigDict
+from typing import List
+from datetime import datetime
+
+
+class Address(BaseModel):
+    street: str
+    city: str
+    zip_code: str
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    is_active: bool = True
+    created_at: datetime
+    address: Address
+    tags: List[str] = []
+
+    model_config = ConfigDict(
+        json_encoders={datetime: lambda v: v.strftime('%d-%m-%Y %H:%M:$S')}
+    )
+
+
+user = User(
+    id=1,
+    name="Hitesh",
+    email="h@hitesh.ai",
+    created_at=datetime(2024, 3 ,15 ,14 ,30),
+    address=Address(
+        street="123 Main St",
+        city="bhagalpur",
+        zip_code="812001"
+    ),
+    is_active=False,
+    tags=["premium", "subscriber"]
+)
+
+python_dict = user.model_dump()
+print(python_dict)
+
